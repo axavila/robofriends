209170a4b9e25cf71a9e78ffd8b523e060b6cfc1
axavila <ax.avila@hotmail.com>
2020-05-21 21:30:52 -0600

    hola

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Hello/>,
   document.getElementById('root')
 );
 
