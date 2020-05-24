051c40d46413f76819e7d279481b4463b163ee23
axavila <ax.avila@hotmail.com>
2020-05-24 11:38:03 -0600

    up2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import Searchbox from './Searchbox';
 import {robots} from './robots';
+import './App.css'
 
-const App =() => {
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+
+        }
+    }
+
+    onSearchChange =(event) => {
+        this.setState({searchfield: event.target.value})
+        
+   
+    }
+    render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
     return (
-        <div>
-            <h1>RoboFriends</h1>
-            <Searchbox/>
-            <CardList robots={robots}/>
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <Searchbox searchChange ={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
 
         </div>
         
 
     );
+
+    }
 }
 export default App;
